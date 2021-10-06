2c75853ca6c731f21e739bed2c149f025470ff89
Spencer <spenclark@hotmail.com>
2021-10-06 13:43:08 -0700

    blogdata

---
+++ src/utils/blogData.jsx
@@ -0,0 +1,67 @@
+export const featuredPost = {
+    title: "InstantCard launches fresh website design",
+    body: "Today, InstantCard is proud to show off a new look to the world! ",
+    media: "https://forgeandsmith.com/wp-content/uploads/2018/03/shutterstock_292036202-1024x704.jpg",
+    tag: "Corporate"
+}
+
+export const posts = [
+    {
+        title: "Including Preferred Pronouns on ID Badges",
+        body: "As the world becomes more accepting of individuals personal preferences we have been seeing an increase in this also being included on employee and school ID badges. Inclusive bosses and institutions are now taking a step further to accommodate employee's preferred pronouns. By including them on employee ID cards individuals can now express their preference in the workplace making for a much more comfortabl",
+        media: "https://instantcard.net/wp-content/uploads/2021/08/studentidpreferredpronouns3.png",
+        tag: "Product"
+    },
+    {
+        title: "Providing Emergency Medical Information in your ID badges",
+        body: "There are many different professions and situations which could warrant needing immediate access to an individuals medical information. Firefighters, construction workers, employees working in hazardous environments in general. Being able to pull an individual’s medical information quickly can be a matter of life and death. At InstantCard we have the ability for your emergency medical information to be provided with your employee ID cards through our Credential Verification Service (CVS).",
+        media: "https://instantcard.net/wp-content/uploads/2016/02/badge.png",
+        tag: "Product"
+
+    },
+    {
+        title: "Extending the life of your ID badges",
+        body: "For some of our clients ID badges are a precious investment. One that you want to last as long as possible. Just like with any other product the more use something gets the more wear and tear befalls it. It’s just the natural course of things and ID badges are not an exception to the rule. I’m here to talk today about helping preserve your ID badge’s for as long as you can before eventually having to replace them.",
+        media: "https://instantcard.net/wp-content/uploads/2021/06/badgewomanprotector-400x585.jpg",
+        tag: "Product"
+
+    },
+    {
+        title: "Why outsource ID card printing?",
+        body: "InstantCard has worked tirelessly to streamline the printing of custom id cards and production. With constant updates to our system and the client portal we have come a long way since the days of our youth. The number 1 goal at InstantCard is to save you time and effort. Who wants to deal with jammed printers? We do! Or at least, we’re well versed in it! Let us handle the nitty gritty of printer maintenance so that you can focus on saving your time and money for what your company does best. Outsourcing a need like ID card printing makes it possible for you to prioritize your end product or service.",
+        media: "https://instantcard.net/wp-content/uploads/2021/06/happy-confident-businesswoman-showing-id-card2.jpg",
+        tag: "Support"
+    },
+    {
+        title: "Is your company still Reliant on Obsolete Access Control Technology?",
+        body: "InstantCard has worked tirelessly to streamline the printing of custom id cards and production. With constant updates to our system and the client portal we have come a long way since the days of our youth. The number 1 goal at InstantCard is to save you time and effort. Who wants to deal with jammed printers? We do! Or at least, we’re well versed in it! Let us handle the nitty gritty of printer maintenance so that you can focus on saving your time and money for what your company does best. Outsourcing a need like ID card printing makes it possible for you to prioritize your end product or service.",
+        media: "https://instantcard.net/wp-content/uploads/2021/06/happy-confident-businesswoman-showing-id-card2.jpg",
+        tag: "Security"
+    },
+    {
+        title: "Troubleshooting Your ID Card Printer",
+        body: "InstantCard has worked tirelessly to streamline the printing of custom id cards and production. With constant updates to our system and the client portal we have come a long way since the days of our youth. The number 1 goal at InstantCard is to save you time and effort. Who wants to deal with jammed printers? We do! Or at least, we’re well versed in it! Let us handle the nitty gritty of printer maintenance so that you can focus on saving your time and money for what your company does best. Outsourcing a need like ID card printing makes it possible for you to prioritize your end product or service.",
+        media: "https://instantcard.net/wp-content/uploads/2021/01/fix-your-printer-scaled.jpg",
+        tag: "Support"
+    },
+    {
+        title: "NonProfit? Have a Discount!",
+        body: "InstantCard has worked tirelessly to streamline the printing of custom id cards and production. With constant updates to our system and the client portal we have come a long way since the days of our youth. The number 1 goal at InstantCard is to save you time and effort. Who wants to deal with jammed printers? We do! Or at least, we’re well versed in it! Let us handle the nitty gritty of printer maintenance so that you can focus on saving your time and money for what your company does best. Outsourcing a need like ID card printing makes it possible for you to prioritize your end product or service.",
+        media: "https://instantcard.net/wp-content/uploads/2019/10/NPO-badge-400x595.png",
+        tag: "Product"
+    },
+    {
+        title: "Is your key card safe?",
+        body: "You are probably not aware that the common “prox” or key cards used to open 100’s of thousands of doors across America can be copied by anyone in a matter of minutes.  Even though these passive prox cards are used by some of America’s largest and most tech-savvy companies, most people will be shocked to learn that these key cards can be easily copied by nearly anyone, in just a few seconds.  Should you be concerned?  If you have responsibility for physical security in a corporate environment, the answer is a resounding “yes”. When the low-frequency (125 kHz) prox technology was introduced in the 1990’s it was considered quite secure at the time.  A combination of many site codes and a large number of unique serial numbers meant that it was highly unlikely that one key card was able to open the doors in an unauthorized building.  But as the number of cards in use proliferated, previously low probability became increasingly higher because there were only 255 unique site codes built into the original card’s technical design.  And to make matters worse, over time more and more people backward-engineered the cards in an effort to compromise their security.",
+        tag: "Security",
+        media: "https://instantcard.net/wp-content/uploads/2020/06/Is-your-key-card-even-safe-768x672.jpg",
+    },
+    {
+        title: "InstantCard announces full integration with the ADP Marketplace",
+        media: "https://instantcard.net/wp-content/uploads/2020/06/banner-e1591608407110.jpg",
+        body: "After nearly six months of development and testing, as well as extensive security validations, InstantCard is pleased to announce that its ID card app is now listed on the ADP Workforce Now® Marketplace, and available to all ADP and InstantCard customers.  This will greatly improve the efficiency with which those corporate clients which use both services will be able to manage their daily operations. Clients are now able to ensure automatic syncing between the master ADP HR database, and the InstantCard ID card database.  New hires, promotions, and terminations which are recorded in the online ADP HR system will be automatically reflected in the InstantCard system.  When a client first signs up, they are presented with an “import” button which allows them to receive into the InstantCard service all data, or a filtered subset of data, from their ADP account.  From that point on, any changes in the cloud-based ADP system will be sent, on a nightly basis, to InstantCard via our API.  However, clients can also at any time click a button to sync changes which have been made to ensure continuous full consistency.",
+        tag: "Corporate"
+    }
+
+
+]
